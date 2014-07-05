3718f7d677fa90eac5585f8f9302b698c1842a2d
kesha <tkesh11@freeuni.edu.ge>
2014-07-05 16:17:05 +0400

    sawyisi vizuali

--- src/com/example/final_project/MainActivity.java
+++ src/com/example/final_project/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.final_project;
 
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,4 +13,10 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+//	public void onTsuClick(View v) {
+//		Intent i = new Intent(getBaseContext(), CategoryActivity.class);
+//		i.putExtra("name", "tsu");
+//		startActivity(i);
+//	}
 }
