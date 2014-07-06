cdcbed3c66f39eca06f3b70829a1c4a3bcf21a3d
kesha <tkesh11@freeuni.edu.ge>
2014-07-06 13:42:20 +0400

    credit and gpa counter

--- src/com/example/final_project/StudentsPageActivity.java
+++ src/com/example/final_project/StudentsPageActivity.java
@@ -9,6 +9,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 public class StudentsPageActivity extends Activity{
 	private ArrayList<Subject> subjects = new ArrayList<Subject>();
@@ -26,7 +27,35 @@ public class StudentsPageActivity extends Activity{
 		listView = (ListView) findViewById(R.id.subjects_list);
 		adapter = new SubjectListAdapter(getLayoutInflater(), ap.getSubjects());
 		listView.setAdapter(adapter);
+		calculate();
+		
 	}
+	
+	private double gpaCredit(double m) {
+		if(m > 90) return 4.0;
+		if(m > 80) return 3.38;
+		if(m > 70) return 2.77;
+		if(m > 60) return 2.16;
+		if(m > 50) return 1.55;
+		
+		return 0;
+	}
+	
+	private void calculate() {
+		int totalCredits = 0;
+		double totalGpaMarks = 0.0;
+		for (int i = 0; i < subjects.size(); i++) {
+			totalCredits = totalCredits + subjects.get(i).getCredits();
+			totalGpaMarks = totalGpaMarks + (subjects.get(i).getCredits() * gpaCredit(subjects.get(i).getPercent()));
+		}
+		TextView t = (TextView)(findViewById(R.id.totalCredits));
+		t.setText(t.getText().toString() + totalCredits);
+		
+		TextView t2 = (TextView)(findViewById(R.id.gpa));
+		double gpa = (totalGpaMarks/totalCredits) * 100;
+		double roundGpa = Math.round(gpa);
+		t2.setText(t2.getText().toString() + roundGpa/100);
+	}	
 	
 	private void addTabs() {
 		TabHost tabhost = (TabHost) findViewById(R.id.tabhost);
