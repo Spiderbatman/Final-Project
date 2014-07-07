189f30eecc51ff5c48c6b1baf343eb5ece330b19
spiderbatman <gigi.pataraia@gmail.com>
2014-07-07 16:45:17 +0400

    removed error

--- gen/com/example/final_project/R.java
+++ gen/com/example/final_project/R.java
@@ -27,24 +27,26 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
         public static final int profile=0x7f020004;
         public static final int subject=0x7f020005;
+        public static final int tsu_logo_svg=0x7f020006;
     }
     public static final class id {
-        public static final int chooseName=0x7f080014;
+        public static final int chooseName=0x7f080016;
         public static final int chooseSubj=0x7f080000;
         public static final int chooseSubjects_list=0x7f080002;
-        public static final int credit=0x7f080017;
+        public static final int credit=0x7f080019;
         public static final int endOfMail=0x7f080005;
         public static final int filterText=0x7f080010;
         public static final int gpa=0x7f08000e;
-        public static final int mark=0x7f080018;
+        public static final int mark=0x7f08001a;
         public static final int mark_spinner=0x7f080012;
         public static final int name=0x7f080003;
         public static final int password=0x7f080006;
-        public static final int select=0x7f080015;
+        public static final int select=0x7f080017;
         public static final int signInBut=0x7f080009;
-        public static final int subjectName=0x7f080016;
+        public static final int subjectName=0x7f080018;
         public static final int subjects_list=0x7f080013;
         public static final int submit=0x7f080001;
+        public static final int surname=0x7f080014;
         public static final int tab1=0x7f08000b;
         public static final int tab2=0x7f08000f;
         public static final int tabhost=0x7f08000a;
@@ -54,20 +56,23 @@ public final class R {
         public static final int userInfo=0x7f08000c;
         public static final int userName=0x7f080004;
         public static final int userPassword=0x7f080007;
+        public static final int userSurName=0x7f080015;
     }
     public static final class layout {
         public static final int activity_choosesubject=0x7f030000;
         public static final int activity_login=0x7f030001;
         public static final int activity_main=0x7f030002;
         public static final int activity_profilepage=0x7f030003;
-        public static final int choosesubjects_list=0x7f030004;
-        public static final int subjects_list=0x7f030005;
+        public static final int activity_tsulogin=0x7f030004;
+        public static final int choosesubjects_list=0x7f030005;
+        public static final int subjects_list=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int avtorization=0x7f040003;
-        public static final int hello_world=0x7f040001;
-        public static final int title=0x7f040002;
+        public static final int avtorization=0x7f040004;
+        public static final int hello_world=0x7f040002;
+        public static final int title=0x7f040003;
+        public static final int tsu=0x7f040001;
     }
     public static final class style {
         /** 
